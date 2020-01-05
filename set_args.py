fb9d335a8098c2e11458fddd6d073e677524dc81
hzl1216 <2101975298@qq.com>
2020-01-05 20:34:49 +0800

    add svhn

--- set_args.py
+++ set_args.py
@@ -3,7 +3,62 @@ import logging
 from util.utils import str2bool
 def create_parser(dataset):
     if dataset is 'cifar10':
-        parser = argparse.ArgumentParser(description='PyTorch ImageNet Training')
+        parser = argparse.ArgumentParser(description='PyTorch cifar10 Training')
+        parser.add_argument('--epochs', default=256, type=int, metavar='N',
+                            help='number of total epochs to run')
+        parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                            help='manual epoch number (useful on restarts)')
+        parser.add_argument('--batch-size', default=64, type=int,
+                            metavar='N', help='labeled-batch size')
+        parser.add_argument('--unsup-ratio', default=5, type=int,
+                            metavar='N', help='The ratio between batch size of unlabeled data and labeled data')
+        parser.add_argument('--lr', '--learning-rate', default=0.002, type=float)
+        parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                            metavar='W', help='weight decay (default: 1e-4)')
+        parser.add_argument('--ema-decay', default=0.999, type=float, metavar='ALPHA',
+                            help='ema variable decay rate (default: 0.999)')
+        parser.add_argument('--consistency-weight', default=1.0, type=float, metavar='WEIGHT',
+                            help='use consistency loss with given weight (default: None)')
+        parser.add_argument('--consistency-type', default="mse", type=str, metavar='TYPE',
+                            choices=['mse', 'kl'],
+                            help='consistency loss type to use')
+        parser.add_argument('--consistency-rampup', default=5, type=int, metavar='EPOCHS',
+                            help='length of the consistency loss ramp-up')
+        parser.add_argument('--entropy-cost', default=0.1, type=float, metavar='WEIGHT')
+        parser.add_argument('--checkpoint-epochs', default=1, type=int,
+                            metavar='EPOCHS', help='checkpoint frequency in epochs, 0 to turn checkpointing off (default: 1)')
+        parser.add_argument('--evaluation-epochs', default=1, type=int,
+                            metavar='EPOCHS', help='evaluation frequency in epochs, 0 to turn evaluation off (default: 1)')
+        parser.add_argument('--print-freq', '-p', default=128, type=int,
+                            metavar='N', help='print frequency (default: 10)')
+        parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                            help='path to latest checkpoint (default: none)')
+        parser.add_argument('--out_path', default='result',
+                            help='Directory to output the result')
+        parser.add_argument('--n-labeled', type=int, default=4000,
+                            help='Number of labeled data')
+        parser.add_argument('-e', '--evaluate', type=bool,
+                            help='evaluate model on evaluation set')
+        parser.add_argument('--num-workers', type=int, default=12,
+                            help='Number of workers')
+        parser.add_argument('--epoch-iteration', type=int, default=1024,
+                            help='train step of one epoch')
+        parser.add_argument('--warmup-step', type=int, default=0,
+                            help='Number of workers')
+        parser.add_argument('--alpha', default=0.75, type=float)
+        parser.add_argument('--mixup', default=True, type=str2bool,
+                            help='use mixup', metavar='BOOL')
+        parser.add_argument('--gpu', default='2', type=str,
+                    help='id(s) for CUDA_VISIBLE_DEVICES')
+        parser.add_argument('--seed', type=int, default=42, help='manual seed')
+        parser.add_argument('--confidence-thresh', default=-1,type=float)
+        parser.add_argument('--scheduler', default='linear')
+        parser.add_argument('--ema-stage', type=int, default=16)
+        parser.add_argument('--optimizer', type=str, default='Adam')
+        parser.add_argument('--mixup-size', type=int, default=5)
+
+    if dataset is 'svhn':
+        parser = argparse.ArgumentParser(description='PyTorch svhn Training')
         parser.add_argument('--epochs', default=256, type=int, metavar='N',
                             help='number of total epochs to run')
         parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
--- util/svhn.py
+++ util/svhn.py
@@ -2,11 +2,10 @@ import numpy as np
 from PIL import Image
 
 import torchvision
-import torch
 
 
 class TransformTwice:
-    def __init__(self, transform1,transform2):
+    def __init__(self, transform1, transform2):
         self.transform1 = transform1
         self.transform2 = transform2
 
@@ -16,23 +15,23 @@ class TransformTwice:
         return out1, out2
 
 
-def get_cifar10(root, n_labeled,
-                transform_normal=None, transform_aug=None,transform_val=None,
+def get_svhn(root, n_labeled,
+                transform_normal=None, transform_aug=None, transform_val=None,
                 download=True):
-    base_dataset = torchvision.datasets.CIFAR10(root, train=True, download=download)
-    train_labeled_idxs, train_unlabeled_idxs, val_idxs = train_val_split(base_dataset.targets, int(n_labeled / 10))
+    base_dataset = torchvision.datasets.SVHN(root, split='train', download=download)
+    train_labeled_idxs, train_unlabeled_idxs, val_idxs = train_val_split(base_dataset.labels, int(n_labeled / 10))
 
-    train_labeled_dataset = CIFAR10_labeled(root, train_labeled_idxs, train=True, transform=transform_normal)
-    train_unlabeled_dataset = CIFAR10_unlabeled(root, train_unlabeled_idxs, train=True,
-                                                transform=TransformTwice(transform_aug,transform_normal))
-    train_unlabeled_dataset2 = CIFAR10_unlabeled(root, train_unlabeled_idxs, train=True,
-                                                transform=transform_val)
+    train_labeled_dataset = SVHN_labeled(root, train_labeled_idxs, split='train', transform=transform_normal)
+    train_unlabeled_dataset = SVHN_unlabeled(root, train_unlabeled_idxs, split='train',
+                                                transform=TransformTwice(transform_aug, transform_normal))
+    train_unlabeled_dataset2 = SVHN_unlabeled(root, train_unlabeled_idxs, split='train',
+                                                 transform=transform_val)
 
-    val_dataset = CIFAR10_labeled(root, val_idxs, train=True, transform=transform_val, download=True)
-    test_dataset = CIFAR10_labeled(root, train=False, transform=transform_val, download=True)
+    val_dataset = SVHN_labeled(root, val_idxs, split='train', transform=transform_val, download=True)
+    test_dataset = SVHN_labeled(root, split='test', transform=transform_val, download=True)
 
     print(f"#Labeled: {len(train_labeled_idxs)} #Unlabeled: {len(train_unlabeled_idxs)} #Val: {len(val_idxs)}")
-    return train_labeled_dataset, train_unlabeled_dataset,train_unlabeled_dataset2, val_dataset, test_dataset
+    return train_labeled_dataset, train_unlabeled_dataset, train_unlabeled_dataset2, val_dataset, test_dataset
 
 
 def train_val_split(labels, n_labeled_per_class):
@@ -53,18 +52,17 @@ def train_val_split(labels, n_labeled_per_class):
 
     return train_labeled_idxs, train_unlabeled_idxs, val_idxs
 
+class SVHN_labeled(torchvision.datasets.SVHN):
 
-class CIFAR10_labeled(torchvision.datasets.CIFAR10):
-
-    def __init__(self, root, indexs=None, train=True,
+    def __init__(self, root, indexs=None, split='train',
                  transform=None, target_transform=None,
                  download=False):
-        super(CIFAR10_labeled, self).__init__(root, train=train,
+        super(SVHN_labeled, self).__init__(root, split=split,
                                               transform=transform, target_transform=target_transform,
                                               download=download)
         if indexs is not None:
             self.data = self.data[indexs]
-            self.targets = np.array(self.targets)[indexs]
+            self.targets = np.array(self.labels)[indexs]
 
     def __getitem__(self, index):
         """
@@ -74,23 +72,24 @@ class CIFAR10_labeled(torchvision.datasets.CIFAR10):
             tuple: (image, target) where target is index of the target class.
         """
         img, target = self.data[index], self.targets[index]
-        img = Image.fromarray(img)
+        img = Image.fromarray(np.transpose(img, (1, 2, 0)))
         if self.transform is not None:
             img = self.transform(img)
 
         if self.target_transform is not None:
             target = self.target_transform(target)
 
-        return img, target,index
+        return img, target, index
 
 
-class CIFAR10_unlabeled(CIFAR10_labeled):
+class SVHN_unlabeled(SVHN_labeled):
 
-    def __init__(self, root, indexs, train=True,
+    def __init__(self, root, indexs, split=True,
                  transform=None, target_transform=None,
                  download=False):
-        super(CIFAR10_unlabeled, self).__init__(root, indexs, train=train,
+        super(SVHN_unlabeled, self).__init__(root, indexs, split=split,
                                                 transform=transform, target_transform=target_transform,
                                                 download=download)
         self.targets = np.array([-1 for i in range(len(self.targets))])
-    
+
+
