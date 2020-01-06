7a5f494ea2402675c454d28337f1d04a65963a58
2101975298@qq.com <hzl1216>
2020-01-06 22:27:28 +0800

    update

---
+++ scripts/cifar10_250_labels.sh
@@ -0,0 +1,1 @@
+python cifar10.py --epochs=150 --warmup-step=15 --optimizer='sgd' --consistency-weight=6.0 --lr=0.03 --weight-decay=0.0007 --scheduler=exp --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
--- scripts/run_250_labels.sh
+++
@@ -1,1 +0,0 @@
-python cifar.py --epochs=150 --warmup-step=15 --optimizer='sgd' --consistency-weight=6.0 --lr=0.03 --weight-decay=0.0007 --scheduler=exp --n-labeled=250 --gpu=1 --batch-size=16 --unsup-ratio=20
