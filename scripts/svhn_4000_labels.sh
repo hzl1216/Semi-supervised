08b65f1022514de3698cd56ef704c455cda580aa
2101975298@qq.com <hzl1216>
2020-01-11 12:40:44 +0800

    update

--- scripts/svhn_4000_labels.sh
+++ scripts/svhn_4000_labels.sh
@@ -1,1 +1,1 @@
-python train.py --dataset=svhn --optimizer='sgd' --warmup-step=20 --lr=0.05 --weight-decay=0.0005 --epochs=400 --gpu=1
+python train.py --dataset=svhn --optimizer='sgd' --warmup-step=15 --lr=0.05 --weight-decay=0.0005 --epochs=150 --gpu=1 --batch-size=32 --unsup-ratio=20  --n-labeled=4000
--- scripts/svhn_all_labels.sh
+++ scripts/svhn_all_labels.sh
@@ -1,7 +1,7 @@
 # 2000, 1000, 500
 for sup_size in  4000 2000 1000 500;
 do
-    python train.py --dataset=svhn --optimizer='sgd' --warmup-step=20 --lr=0.05 --weight-decay=0.0005 --epochs=400 --gpu=1 --n-labeled=${sup_size}
+    python train.py --dataset=svhn --optimizer='sgd' --warmup-step=15 --lr=0.05 --weight-decay=0.0005 --epochs=150 --gpu=1 --batch-size=32 --unsup-ratio=20  --n-labeled=${sup_size}
     $@
 done
 #250
